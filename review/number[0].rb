4c7b1ced0ec817b14baae2037dc9f90b30514f67
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-07-16 03:12:19 +0000

    number[0]

---
+++ review/number[0].rb
@@ -0,0 +1,7 @@
+number = gets.split(' ')
+
+if number[0] == number[1] || number[1] == number[2] || number[2] == number[3] || number[3] == number[4] || number[4] == number[5]
+  puts "YES"
+else
+  puts "NO"
+end
